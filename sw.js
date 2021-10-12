6dd56a75c64e954d7627f6f5c729275e68a4a229
JbrownWFU <92392162+JbrownWFU@users.noreply.github.com>
2021-10-12 18:13:25 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,35 @@
+self.addEventListener('fetch', function(event){
+    event.respondWith(
+        fetch(event.request).catch(function() {
+            return new Response (
+                "Welcome to the news\n" + "Looks like you are offline\n"
+            );
+        })
+    
+        );
+    });
+
+
+
+
+self.addEventListener ('install' , function(event) {
+    event.waitUntil(
+        caches.open('newsCache').then(function(cache) {
+            return cache.addAll(
+
+            ['/offline.html', '/index.html', '/CSS/Styles.css']
+
+            );
+        })
+    );
+});
+
 self.addEventListener('fetch', function(event) {
     console.log(event.request.url);
+
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+        })
+    );
 });
